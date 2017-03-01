4f17121d40202a13e4a2df5165ecb978f5d86b58
VKMUSTH <valerykempf@gmail.com>
2017-03-01 15:19:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ mysql_select_db($database);
 	<title>Musth &#8250; Accueil</title>
 	<link href="musth.co/public/stylesheet/musth.css" 	rel="stylesheet" 	type="text/css">
 	<link href="musth.co/public/stylesheet/print.css" 	rel="stylesheet" 	type="text/css" media="print">
-	<link href="musth.co/public/images/favicon/icn.musth.hover.png"	rel="icon" 		type="image/png"/>
+	<link href="https://cloud.githubusercontent.com/assets/22053566/23463399/9a964af2-fe91-11e6-9f00-5060337cf164.png"	rel="icon" 		type="image/png"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<div class="logo" align=center><a href="http://musth.co.tld/" >	<img rel="icon" alt="Musth" src="https://cloud.githubusercontent.com/assets/22053566/23463411/a7c885fa-fe91-11e6-9172-c0b1a8e15e6e.png"/></a> 
 </head>
