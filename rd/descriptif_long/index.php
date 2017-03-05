cd3a1b12acfe30774050b59ffa1c5cf07baed308
VKMUSTH <valerykempf@gmail.com>
2017-03-06 00:24:55 +0100

    Create index.php

---
+++ rd/descriptif_long/index.php
@@ -0,0 +1,7 @@
+	<?php require "../../header.php"; ?>
+	<?php
+	$sql = 'SELECT * FROM admin WHERE id = \'1\'';
+	$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+	while($admin = mysql_fetch_assoc($req)) { ?>
+	<?php include ''.$admin['numproduit'].'/'.$admin['numdossier'].'/admin.php'; ?>
+	<?php } mysql_close(); ?>
