180ebef751d79301fca33f93ff44cd6539f81843
VKMUSTH <valerykempf@gmail.com>
2017-03-07 00:30:40 +0100

    Update connexion_sql.php

--- connexion_sql.php
+++ connexion_sql.php
@@ -5,3 +5,4 @@ $db = mysql_connect('localhost', 'root', 'votre_mdp');
 // on sélectionne la base
 mysql_select_db('Musth',$db);
 ?>
+<?php try {$bdd = new PDO('mysql:host=localhost;dbname=Musth', 'root', 'votre_mdp');} catch(Exception $e) {die('Erreur : '.$e->getMessage());} ?>
