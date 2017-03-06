3fbb26c28915e04d8ceb34834c6b2174db4343e3
VKMUSTH <valerykempf@gmail.com>
2017-03-07 00:45:31 +0100

    Create admin3.php

---
+++ dossier_financier/previsions/admin3.php
@@ -0,0 +1,76 @@
+	<table border=0 cellpadding=0 cellspacing=0  >
+		<tr class="niv2" style="font-weight : bold;">
+			<td width=10%>
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Jan</button></td>
+					<input type="text"  name="mois_en_cours" value="01"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Févr</button></td>
+					<input type="text"  name="mois_en_cours" value="02"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Mars</button></td>
+					<input type="text"  name="mois_en_cours" value="03"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Avr</button></td>
+					<input type="text"  name="mois_en_cours" value="04"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Mai</button></td>
+					<input type="text"  name="mois_en_cours" value="05"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Juin</button></td>
+					<input type="text"  name="mois_en_cours" value="06"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Juil</button></td>
+					<input type="text"  name="mois_en_cours" value="07"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Août</button></td>
+					<input type="text"  name="mois_en_cours" value="08"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Sept</button></td>
+					<input type="text"  name="mois_en_cours" value="09"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Oct</button></td>
+					<input type="text"  name="mois_en_cours" value="10"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Nov</button></td>
+					<input type="text"  name="mois_en_cours" value="11"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+
+					<form name="mois" action="previsions/set_mois.php"  method="post">
+			<td width=7.5%><button type="submit" class="button" >Déc</button></td>
+					<input type="text"  name="mois_en_cours" value="12"  id="mois_en_cours" hidden/>
+					<input type="text" name="annee_en_cours" value="<?php echo $admin['annee_en_cours']  ; ?>" id="annee_en_cours" hidden/>
+					</form>
+		</tr>
+	</table>
