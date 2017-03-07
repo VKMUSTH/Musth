c16530a346a617e83dbca7daad240bcb6da4774e
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:57:15 +0100

    Create signatures.php

---
+++ operations/vouchers/client_vouchers/top_voucher/signatures.php
@@ -0,0 +1,19 @@
+	<!--SIGNATURES-->
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR VALIGN=TOP>
+			<TD STYLE="width:50%; border-bottom: 1px solid #b8bec3;border-top: 1px solid #b8bec3; border-left: 1px solid #b8bec3" HEIGHT=170 >
+					<script type="text/javascript"> 
+						d = new Date(); 
+					      document.write("<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Obernai, le : <span style='color:#8aa;'>"); 
+					      document.write(d.toLocaleDateString()); 
+					      document.write("</span></p>"); 
+					</script>
+				</TD>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-top: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >
+				<P><BR>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Musth: Services touristiques :</P>
+			</TD>
+		</TR>
+		<TR>
+			<TD HEIGHT=16 ><BR></TD>
+		</TR>
+	</TABLE>
