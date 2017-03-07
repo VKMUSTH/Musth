74285eca09302edd21b0460abd52d721c31f9ec9
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:39:22 +0100

    Create assurances.php

---
+++ operations/bi/client/assurances.php
@@ -0,0 +1,24 @@
+	<TABLE class="assurances" FRAME=VOID CELLSPACING=0 RULES=NONE >
+		<TR COLSPAN=5 BGCOLOR="#d8dfea">
+			<TD STYLE="width:100%" COLSPAN=3 ALIGN=CENTER VALIGN=MIDDLE >		<B>ASSURANCES</B></TD>
+		</TR>
+		<TR HEIGHT=16 ALIGN=LEFT BGCOLOR="#eff2f7">
+			<TD STYLE="width:20%" STYLE="border-left: 1px solid #b8bec3"  >		<B>Assureur</B></TD>
+			<TD STYLE="width:50%" >							<B>Formule</B></TD>
+			<TD STYLE="width:30%" STYLE="border-right: 1px solid #b8bec3" >		<B>N° contrat</B></TD>
+		</TR>
+		<TR ALIGN=CENTER>
+			<TD STYLE="border-left: 1px solid #b8bec3" >			<input type="text" name="" value="<?php echo $donnees9['assureur']; ?>" class="" id=""/>		</TD>
+			<TD>								<input type="text" name="" value="<?php echo $donnees9['formule']; ?>" class="" id=""/>			</TD>
+			<TD STYLE="border-right: 1px solid #b8bec3">			<input type="text" name="" value="<?php echo $donnees9['numerocontrat']; ?>" class="" id=""/>		</TD>
+		</TR>
+		<TR ALIGN=LEFT>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" ><BR>						</TD>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-right: 1px solid #b8bec3" ><BR>										</TD>
+			<TD COLSPAN=3 STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" ><BR>
+						<input type="checkbox" name="option2" value="Butter" checked> Assurance refusée par le client<br>	</TD>
+		</TR>
+		<TR ALIGN=LEFT>
+			<TD><BR></TD>
+		</TR>
+	</TABLE>	
