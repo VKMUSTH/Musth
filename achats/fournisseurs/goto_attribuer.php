9fd5fde2c252c9b341b0c7630ce049ce6e07e24c
VKMUSTH <valerykempf@gmail.com>
2017-03-07 10:39:08 +0100

    Create goto_attribuer.php

---
+++ achats/fournisseurs/goto_attribuer.php
@@ -0,0 +1,11 @@
+<?php require "../../connexion_sql.php"; ?>
+<?php
+$set_numcommande = $bdd->prepare('UPDATE admin SET numcommande = :nv_numcommande WHERE id = \'1\'');
+$set_numcommande->execute(array('nv_numcommande' => $_POST['numcommande']));
+
+$set_numcontact = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
+$set_numcontact->execute(array('nv_numcontact' => $_POST['numcontact']));
+
+
+header('Location: attribuer');
+?>
