df3ed7224e061c89b1c805a8108e3923d8092c86
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:41:54 +0100

    Create formalites.php

---
+++ operations/bi/client/formalites.php
@@ -0,0 +1,29 @@
+	<br>
+	<TABLE FRAME=VOID CELLSPACING=0 RULES=NONE >
+		<TR class="niv1"  VALIGN=MIDDLE>
+			<TD COLSPAN=4 ALIGN=CENTER >							<a href="../../../rd/formalites"><B>FORMALITES</B></a>					</TD>
+		</TR>
+		<TR ALIGN=CENTER class="niv2">
+			<TD STYLE="width:50%; border-left: 1px solid #b8bec3" COLSPAN=2 >		<B>POLICE</B>								</TD>
+			<TD STYLE="width:50%; border-right: 1px solid #b8bec3" COLSPAN=2 >		<B>SANTE</B>								</TD>
+		</TR>
+		<TR class="niv3" ALIGN=LEFT>
+			<TD STYLE="border-left: 1px solid #b8bec3" >					Formalité&nbsp;:							</TD>
+			<TD STYLE="" >										<?php echo $donnees12['policeformalite']; ?>			</TD>
+			<TD STYLE="" >									Formalité&nbsp;:							</TD>
+			<TD STYLE="border-right: 1px solid #b8bec3"  >						<?php echo $donnees12['santeformalite']; ?>			</TD>
+		</TR>
+		<TR class="niv3" ALIGN=LEFT>
+			<TD STYLE="border-left: 1px solid #b8bec3" >					Validité&nbsp;:								</TD>
+			<TD>											<?php echo $donnees12['policevalidite']; ?>			</TD>
+			<TD>										Obligatoire/Recommandée&nbsp;:						</TD>
+			<TD STYLE="border-right: 1px solid #b8bec3" >						<?php echo $donnees12['santeobligrecom']; ?>			</TD>
+		</TR>
+		<TR class="niv3" HEIGHT=26 ALIGN=JUSTIFY>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" COLSPAN=4 ><I>
+				<FONT SIZE=1>Pour les non-ressortissants français, prière de consulter les autorités compétentes. Il appartient aux voyageurs de vérifier la validité des documents et vaccins ci-dessus mentionnés, 					ainsi que l'orthographe des noms et prénoms figurants sur ce bulletin.</FONT></I></TD>
+		</TR>
+		<TR HEIGHT=16 ALIGN=LEFT>
+			<TD><BR></TD>
+		</TR>
+	</TABLE>
