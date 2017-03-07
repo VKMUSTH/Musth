91d1fda04fe112c20388b85d4d122a5cd00521a7
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:40:54 +0100

    Create conditions_annulation.php

---
+++ operations/bi/client/conditions_annulation.php
@@ -0,0 +1,21 @@
+	<!--CONDITIONS D'ANNULATION - RÉVISION DU PRIX-->
+<br>
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR class="niv1" VALIGN=MIDDLE >
+			<TD STYLE="width:50%; font-weight : bold;" HEIGHT=16 ALIGN=CENTER  >					<a><B>CONDITIONS D'ANNULATION</B></a></TD>
+			<TD style="font-weight : bold;" ALIGN=CENTER >								<a><B>REVISION DU PRIX</B></a></TD>
+		</TR>
+		<TR class="niv3" ALIGN=LEFT>
+			<TD STYLE="border-left: 1px solid #b8bec3" HEIGHT=30 >				Selon les Conditions Particulières de Vente indiquées dans la brochure ou le programme de l'organisateur identifié.</TD>
+			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >
+														Selon les Conditions Particulières de Vente indiquées dans la brochure ou le programme de l'organisateur identifié.</TD>
+		</TR>
+		<TR class="niv3" HEIGHT=16 ALIGN=LEFT> 
+			<TD STYLE="border-left: 1px solid #b8bec3" >					Conditions contractuelles</TD>
+			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >	Conditions contractuelles</TD>
+		</TR>
+		<TR class="niv3" HEIGHT=16 ALIGN=LEFT>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3" >	<BR></TD>
+			<TD STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" ><BR></TD>
+		</TR>
+	</TABLE>
