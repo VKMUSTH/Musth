807d48b0c312bb24dfe7c48b617e8bc51448ab9b
VKMUSTH <valerykempf@gmail.com>
2017-03-07 01:52:40 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <html>
 <head>
 	<title>Musth &#8250; Accueil</title>
-	<link href="../../../musth.co/public/stylesheet/musth.css" 	rel="stylesheet" 	type="text/css">
-	<link href="../../../musth.co/public/stylesheet/print.css" 	rel="stylesheet" 	type="text/css" media="print">
-	<link href="../../../musth.co/public/images/favicon/icn.musth.hover.png"	rel="icon" 		type="image/png"/>
+	<link href="../../../../../musth.co/public/stylesheet/musth.css" 	rel="stylesheet" 	type="text/css">
+	<link href="../../../../../musth.co/public/stylesheet/print.css" 	rel="stylesheet" 	type="text/css" media="print">
+	<link href="../../../../../musth.co/public/images/favicon/icn.musth.hover.png"	rel="icon" 		type="image/png"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<div class="logo" align=center><a href="http://musth.co.tld/" >	<img rel="icon" alt="Musth" src="../../../musth.co/public/images/logo/musth_logos.png"/></a> 
+	<div class="logo" align=center><a href="http://musth.co.tld/" >	<img rel="icon" alt="Musth" src="../../../../../musth.co/public/images/logo/musth_logos.png"/></a> 
 	<?php
 	// on se connecte à MySQL
 	include "connexion_sql.php";
