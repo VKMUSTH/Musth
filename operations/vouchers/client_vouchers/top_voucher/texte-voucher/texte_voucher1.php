3f003e54443d1e14881775548ae725479fdde47b
VKMUSTH <valerykempf@gmail.com>
2017-03-07 07:02:31 +0100

    Create texte_voucher1.php

---
+++ operations/vouchers/client_vouchers/top_voucher/texte-voucher/texte_voucher1.php
@@ -0,0 +1,5 @@
+	<!--TEXTE VOUCHER -->
+	<table border=0 cellpadding=0 cellspacing=0 >
+			<TR STYLE="width:100%" HEIGHT=44 ALIGN=JUSTIFY>
+				<TD><P>Cher(e) <?php echo $contact['civilite']; ?> <?php echo $contact['nom']; ?>,</P></TD>
+			</TR>
