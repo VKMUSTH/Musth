8af12b967222e58e8bc95485341be6443bfbb9d5
VKMUSTH <valerykempf@gmail.com>
2017-03-07 09:59:33 +0100

    Create goto_contact.php

---
+++ operations/annulation/client/goto_contact.php
@@ -0,0 +1,10 @@
+<?php require "../../../connexion_sql.php"; ?>
+<?php
+$set_numvoyageur = $bdd->prepare('UPDATE admin SET numvoyageur = :nv_numvoyageur WHERE id = \'1\'');
+$set_numvoyageur->execute(array('nv_numvoyageur' => $_POST['numvoyageur']));
+
+$set_numcontact = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
+$set_numcontact->execute(array('nv_numcontact' => $_POST['numcontact']));
+
+header('Location: ../../../communication/contact/');
+?>
