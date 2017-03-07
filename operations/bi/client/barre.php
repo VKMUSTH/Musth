4d1b4f286588dbd9eaf66f54672583cce8483e78
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:39:52 +0100

    Create barre.php

---
+++ operations/bi/client/barre.php
@@ -0,0 +1,7 @@
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR class="niv1" >
+			<TD style="width:50%"><a><B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BULLETIN D'INSCRIPTION N°:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</B></a><?php echo $client['numclient']  ; ?>		</TD>
+			<TD style="width:40%" ALIGN=RIGHT >		<a><B>CONTRAT DE VENTE EN DATE DU:	</B></a>									</TD>
+			<TD style="width:10%" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $client['date_cdv']; ?>											</TD>
+		</TR>
+	</TABLE>
