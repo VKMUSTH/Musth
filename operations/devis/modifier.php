1a40d8eb47f70567d1153aba12a990b3585e3c48
VKMUSTH <valerykempf@gmail.com>
2017-03-07 10:30:57 +0100

    Create modifier.php

---
+++ operations/devis/modifier.php
@@ -0,0 +1,10 @@
+<?php require "../../connexion_sql.php"; ?>
+<?php
+$set_admin = $bdd->prepare('UPDATE admin SET 
+	numclient = :nv_numclient
+		 WHERE id = \'1\'');
+$set_admin->execute(array(
+	'nv_numclient' => $_POST['numclient']
+		));
+header('Location: modifier');
+?>
