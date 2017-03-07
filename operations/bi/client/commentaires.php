bd8c5a924c72f0c0132888904db02335ff1219d5
VKMUSTH <valerykempf@gmail.com>
2017-03-07 06:40:23 +0100

    Create commentaires.php

---
+++ operations/bi/client/commentaires.php
@@ -0,0 +1,74 @@
+	<!--COMMENTAIRES-->
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR class="niv1" VALIGN=MIDDLE>	<TD ALIGN=CENTER>								<a><B>COMMENTAIRES</B></a>		</TD></TR>
+		<TR class="niv3" ALIGN=LEFT>			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >	<B>CONDITIONS D'ANNULATIONS</B>		</TD></TR>
+		<TR class="niv3" ALIGN=LEFT>			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >	<BR>					</TD></TR>
+		<TR class="niv3"ALIGN=LEFT>			
+			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >
+				Jusqu'à 31 jours avant le départ&nbsp;: 20 % de frais		<br>	
+				De 30 jours à 23 jours avant le départ&nbsp;: 25% de frais	<br>
+				De 22 jours à 16 jours avant le départ&nbsp;: 35% de frais	<br>
+				De 15 jours à 9 jours avant le départ&nbsp;: 50% de frais	<br>
+				De 8 jours à 3 jours avant le départ&nbsp;: 70% de frais	<br>
+				De 2 jours au jour du départ&nbsp;: 80% de frais		<br>
+		<TR class="niv3" ALIGN=LEFT>			<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >	<BR>					</TD></TR>
+		<TR class="niv3" ALIGN=LEFT>			<TD STYLE="border-bottom: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >
+				CHANGEMENT DE NOM POSSIBLE AVEC FRAIS (ENVIRON 100 EUROS) TANT QUE LES CARNETS DE VOYAGE NE SONT PAS ETABLIS.				</TD></TR>
+	</TABLE>
+
+<br>
+	<table border=0 cellpadding=0 cellspacing=0 >
+
+			<TR STYLE="width:100%" BGCOLOR="#d8dfea">
+				<TD HEIGHT=16 ALIGN=CENTER VALIGN=MIDDLE ><B></B></TD></TR>
+			<TR>
+				<TD HEIGHT=44 ALIGN=JUSTIFY>Je soussigné(e) ...................................................................  ............................................................... agissant pour 					moi-même et/ou pour le compte des autres personnes inscrites, certifie avoir pris connaissance des Conditions Générales de Vente de voyages ci-jointes. Je reconnais avoir reçu la brochure et/ou le 					devis, proposition, programme de l'organisateur mentionné ci-dessus contenant les Conditions Particulières de Vente ainsi que les conditions d'annulations inhérentes. Je reconnais avoir reçu les 					conditions et garanties des assurances souscrites.</TD>
+			</TR>
+			<TR>
+				<TD HEIGHT=16 ALIGN=LEFT><I><FONT SIZE=1>Les conditions générales issues des art. R211-5 à R211-13 du code du tourisme ne s'appliquent qu'a la vente de forfait touristique.</FONT></I></TD>
+			</TR>
+
+		</TABLE>
+	<!--SIGNATURES-->
+<br>
+	<table border=0 cellpadding=0 cellspacing=0 >
+			<TR class="niv3"STYLE="width:100%" HEIGHT=16 ALIGN=CENTER VALIGN=MIDDLE >
+				<TD STYLE="width:50%; border-top: 1px solid #b8bec3; border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3"  ><B></B></TD>
+				<TD STYLE="border-top: 1px solid #b8bec3; border-right: 1px solid #b8bec3"  ><B></B></TD></TR>				
+			<TR class="niv3"HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<B>Le:</B></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<B>Vendeur:</B></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		Signature du client :</TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		« lu et approuvé » (mention manuscrite)</TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3" >		<BR></TD>				
+			</TR>
+			<TR class="niv3" HEIGHT=16 ALIGN=LEFT >
+				<TD STYLE="border-left: 1px solid #b8bec3; border-right: 1px solid #b8bec3; border-bottom: 1px solid #b8bec3;" >		<BR></TD>
+				<TD STYLE="border-right: 1px solid #b8bec3; border-bottom: 1px solid #b8bec3;" >		<BR></TD>				
+			</TR>
+		</TABLE>
