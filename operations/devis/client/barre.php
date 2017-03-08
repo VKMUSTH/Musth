1315bc36654177fccc04560b741d0cde751c1b38
VKMUSTH <valerykempf@gmail.com>
2017-03-08 02:16:12 +0100

    Create barre.php

---
+++ operations/devis/client/barre.php
@@ -0,0 +1,5 @@
+	<!--BARRE-->	<!--CONFIRMER DEVIS-->
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR class="niv1"  >
+			<TD>	<a href="../../../communication/contact"><B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DEVIS N°: <span class=red><?php echo $admin['numclient']; ?></span></B></a></TD>		
+	</table>
