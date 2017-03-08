36728fcb8944bbb9d5a7c8ad655dc193fa057769
VKMUSTH <valerykempf@gmail.com>
2017-03-08 02:17:56 +0100

    Create cachet_signature.php

---
+++ operations/devis/client/cachet_signature.php
@@ -0,0 +1,10 @@
+	<!--CACHET ET SIGNATURE-->
+
+	<table border=0 cellpadding=0 cellspacing=0 >
+			<TR BGCOLOR="#d8dfea"><TD HEIGHT=16 ALIGN=CENTER ><B><BR></B></TD>			</TR>
+			<TR><TD >Nous restons à votre disposition pour toute question supplémentaire et vous souhaitons un agréable séjour.</TD>			</TR>
+			<TR><TD >Avec nos meilleures salutations.</TD>			</TR>
+
+			<TR><TD >Valéry KEMPF</TD>		</TR>
+	</table>
+<br>
