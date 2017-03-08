f258bc76961a192c03ebd46e2dfba2f58cb7868f
VKMUSTH <valerykempf@gmail.com>
2017-03-08 02:31:25 +0100

    Create modifier.php

---
+++ operations/devis/client/remarque_prix/les_prix_ne_comprennent_pas/modifier.php
@@ -0,0 +1,8 @@
+<?php require "../../../../../connexion_sql.php"; ?>
+<?php
+
+$req = $bdd->prepare('UPDATE admin SET position = :nv_position WHERE id = \'1\'');
+$req->execute(array('nv_position' => $_POST['position']	));
+
+header('Location: modifier');
+?>
