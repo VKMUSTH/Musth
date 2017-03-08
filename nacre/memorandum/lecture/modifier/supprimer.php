28c6a4e40d7179479506364f9334313b612f1c70
VKMUSTH <valerykempf@gmail.com>
2017-03-08 04:25:27 +0100

    Create supprimer.php

---
+++ nacre/memorandum/lecture/modifier/supprimer.php
@@ -0,0 +1,16 @@
+<?php require "../../../../connexion_sql.php"; ?>
+<?php
+$req = $bdd->prepare('UPDATE admin SET position = :nv_position WHERE id = \'1\'');
+$req->execute(array('nv_position' => $_POST['position']));
+
+$req1 = $bdd->prepare('SELECT position FROM admin WHERE id = \'1\'');
+$req1->execute(array($position['position']	));
+			while ($position = $req1->fetch()){
+
+$req2 = $bdd->prepare('DELETE FROM annexes WHERE position = '.$position['position'].' ');
+$req2->execute(array($position['position']	));
+
+}$req1->closeCursor();
+
+header('Location: ../../lecture');
+?>
