4e7c38509f5590bf29190d46616b8003c6b90437
VKMUSTH <valerykempf@gmail.com>
2017-03-08 04:56:56 +0100

    Create modifier.php

---
+++ communication/courrier/modifier.php
@@ -0,0 +1,15 @@
+<?php require "../../connexion_sql.php"; ?>
+<?php
+
+$req = $bdd->prepare('UPDATE admin SET 
+	position = :nv_position,
+	numcontact = :nv_numcontact
+		 WHERE id = \'1\'');
+$req->execute(array(
+	'nv_position' => $_POST['position'],
+	'nv_numcontact' => $_POST['numcontact']
+		));
+
+
+header('Location: modifier');
+?>
