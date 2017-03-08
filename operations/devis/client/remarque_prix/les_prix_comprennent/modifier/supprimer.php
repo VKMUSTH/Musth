d15a6b1969d7075857f0e8c9e50650c31d06ff0b
VKMUSTH <valerykempf@gmail.com>
2017-03-08 02:29:02 +0100

    Create supprimer.php

---
+++ operations/devis/client/remarque_prix/les_prix_comprennent/modifier/supprimer.php
@@ -0,0 +1,11 @@
+<?php require "../../../../../../connexion_sql.php"; ?>
+<?php
+$select = $bdd->prepare('SELECT position FROM admin WHERE id = \'1\'');
+$select->execute(array($position['position']	));
+			while ($position = $select->fetch()){
+$supprimer = $bdd->prepare('DELETE FROM remarques_prix WHERE position = '.$position['position'].' ');
+$supprimer->execute(array($position['position'],	));
+
+}$select->closeCursor();
+header('Location: ..');
+?>
