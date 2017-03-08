ed668dd48dc45135063ed798116c8c8f90ef1dbf
VKMUSTH <valerykempf@gmail.com>
2017-03-08 03:31:31 +0100

    Create modifier_growth.php

---
+++ nacre/growth/modifier_growth.php
@@ -0,0 +1,12 @@
+<?php require "../../connexion_sql.php"; ?>
+<?php
+$modifier = $bdd->prepare('UPDATE admin SET 
+	position = :nv_position,
+	numcontact = :nv_numcontact
+		 WHERE id = \'1\'');
+$modifier->execute(array(
+	'nv_position' => $_POST['position'],
+	'nv_numcontact' => $_POST['numcontact']
+		));
+header('Location: modifier_growth');
+?>
