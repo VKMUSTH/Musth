c36b0f24d8d6159fc7483ce23fbb963100af758a
VKMUSTH <valerykempf@gmail.com>
2017-03-08 01:13:44 +0100

    Create index.php

---
+++ produit/restauration/modifier/index.php
@@ -0,0 +1,68 @@
+	<?php require "../../../header.php"; ?>
+	<?php
+	$sql = 'SELECT * FROM admin WHERE id = \'1\'';
+	$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+	while($admin = mysql_fetch_assoc($req)) { ?>
+	<?php if (isset(
+	$admin['numdossier'],
+	$admin['position'],
+	$admin['numcontact']
+	) ) {?>
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<tr valign=top  >
+		<?php
+		$sql1 = 'SELECT * FROM dossiers WHERE numdossier = '.$admin['numdossier'].'';
+		$req1 = mysql_query($sql1) or die('Erreur SQL !<br>'.$sql1.'<br>'.mysql_error());
+		while($name = mysql_fetch_assoc($req1)) { ?>	
+			<td colspan=6>	<h1><?php echo $name['titre']; ?></h1>															</td>
+		<?php } //$get_name ?>
+			<td class=inputnum><label><a href="">Date départ du circuit</a></label>
+				<input type="text" name="numcontact" value="dpt+jour-1"  id="numcontact"  />
+
+			<td class=inputnum><label><a href="">N° Client</a></label><input type="text" name="numclient" value="<?php echo $admin['numclient']  ; ?>" id="numclient" />
+				<form  action="supprimer.php"  method="post">
+			<input type="submit" value="Supprimer"   />
+				</form>
+			</td>
+			<td class=inputnum><label><a href="../../../communication/contact">N° contact</a></label>
+				<input type="text" name="numcontact" value="<?php echo $admin['numcontact']  ; ?>"  id="numcontact"  />
+
+			<form  action="modifier.php"  method="post">
+			<input type="submit" value="Enregistrer"   />
+			<input type="text"  value="<?php echo $admin['numcontact']  ; ?>" name="numcontact" id="numcontact" hidden />
+	</table>
+	<BR>
+	<table border=0 cellpadding=0 cellspacing=0 >
+		<TR class="niv1">
+			<TD  COLSPAN=4 ALIGN=CENTER VALIGN=MIDDLE >				<B>RESTAURATION</B>										</TD>	
+		</TR>
+		<TR class="niv2" HEIGHT=16 ALIGN=LEFT >
+			<TD STYLE="width:5%;  " >						<B>Jour</B>											</TD>
+			<TD STYLE="width:85%;  " >						<B>Prestation</B>										</TD>
+			<TD STYLE="width:10%;  " >						<B>Horaire</B>											</TD>
+		</TR>
+		<?php
+		$sql2 = 'SELECT 
+			position,
+			numcontact, 
+			jours,
+			designation,
+			horaire 
+				FROM synoptique 
+			WHERE position = '.$admin['position'].' 
+			';
+		$req2 = mysql_query($sql2) or die('Erreur SQL !<br>'.$sql2.'<br>'.mysql_error());
+		while($restauration = mysql_fetch_assoc($req2)) { ?>	
+
+		<TR ALIGN=LEFT>
+			<TD>	<input type="text" value="<?php echo $restauration['jours']  ; ?>" name="jours" id="jours"  />									</TD>
+			<TD>	<input type="text" value="<?php echo $restauration['designation']  ; ?>" name="designation" id="designation"  />						</TD>
+			<TD>	<input type="text" value="<?php echo $restauration['horaire']  ; ?>" name="horaire" id="horaire"  />								</TD>
+		</TR>
+			</form>
+		<?php } //$get_restauration ?>
+	</table>
+	<?php } // isset ?>
+	<?php
+	    } mysql_close();
+	?>
