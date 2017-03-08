1e1b6d55d78d71481f3f429760c8d1e44a0f7702
VKMUSTH <valerykempf@gmail.com>
2017-03-08 03:48:39 +0100

    Create supprimer.php

---
+++ nacre/developpement/modifier/supprimer.php
@@ -0,0 +1,16 @@
+<?php require "../../../connexion_sql.php"; ?>
+<?php
+$modifier_position = $bdd->prepare('UPDATE admin SET position = :nv_position WHERE id = \'1\'');
+$modifier_position->execute(array('nv_position' => $_POST['position']));
+
+
+$get_position = $bdd->prepare('SELECT position FROM admin WHERE id = \'1\'');
+$get_position->execute(array($position['position']	));
+			while ($position = $get_position->fetch()){
+
+	$supprimer = $bdd->prepare('DELETE FROM retroplanning WHERE position = '.$position['position'].' ');
+	$supprimer->execute(array($position['position']	));
+
+}$get_position->closeCursor();
+header('Location: ..');
+?>
