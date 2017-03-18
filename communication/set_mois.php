12526bc90ca946ca23238649934686a6cf2cc0af
VKMUSTH <valerykempf@gmail.com>
2017-03-18 17:04:19 +0100

    Create set_mois.php

---
+++ communication/set_mois.php
@@ -0,0 +1,13 @@
+<?php require "../connexion_sql.php"; ?>
+<?php
+$set_donnees = $bdd->prepare('UPDATE admin SET 
+	mois_en_cours = :nv_mois_en_cours,
+	annee_en_cours = :nv_annee_en_cours
+		 WHERE id = \'1\'');
+$set_donnees->execute(array(
+	'nv_mois_en_cours' => $_POST['mois_en_cours'],
+	'nv_annee_en_cours' => $_POST['annee_en_cours']
+		));
+
+header('Location: ../communication');
+?>
