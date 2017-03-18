421348fddd0e8b2e18ef7180dc2281ed81c2c062
VKMUSTH <valerykempf@gmail.com>
2017-03-18 20:00:31 +0100

    Update header_tbb.php

--- header_tbb.php
+++ header_tbb.php
@@ -5,7 +5,7 @@
 	<link href="musth.co/public/stylesheet/print.css" 	rel="stylesheet" 	type="text/css" media="print">
 	<link href="https://cloud.githubusercontent.com/assets/22053566/23463399/9a964af2-fe91-11e6-9f00-5060337cf164.png"	rel="icon" 		type="image/png"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<div class="logo" align=center><a href="http://musth.co.tld/" >	<img rel="icon" alt="Musth" src="https://cloud.githubusercontent.com/assets/22053566/23463411/a7c885fa-fe91-11e6-9172-c0b1a8e15e6e.png"/></a> 
+	<div class="logo" align=center><a href="http://musth.co.tld/Musth-master" >	<img rel="icon" alt="Musth" src="https://cloud.githubusercontent.com/assets/22053566/23463411/a7c885fa-fe91-11e6-9172-c0b1a8e15e6e.png"/></a> 
 </head>
 
 	<body class=white bgcolor=#ffffff>
