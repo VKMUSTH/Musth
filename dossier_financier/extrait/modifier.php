bce1939cee20fad656ef401e532a8d4375772a47
VKMUSTH <valerykempf@gmail.com>
2018-10-24 04:45:04 +0200

    Delete modifier.php

--- dossier_financier/extrait/modifier.php
+++
@@ -1,7 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php
-
-$actualiser_numinscription = $bdd->prepare('UPDATE admin SET numinscription = :nv_numinscription WHERE id = \'1\'');
-$actualiser_numinscription->execute(array('nv_numinscription' => $_POST['numinscription']));
-header('Location: modifier');
-?>
