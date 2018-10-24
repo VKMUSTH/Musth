a2011e6c7a3eeadedfad70217acf3feee2d5496e
VKMUSTH <valerykempf@gmail.com>
2018-10-24 04:43:47 +0200

    Delete goto_mailing.php

--- communication/contact/goto_mailing.php
+++
@@ -1,7 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php
-$select = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
-$select->execute(array('nv_numcontact' => $_POST['numcontact'],));
-
-header('Location: ../../operations/facture/mailing');
-?>
