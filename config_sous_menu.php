05948b5fe6372985581f9c0295374de7761f9754
VKMUSTH <valerykempf@gmail.com>
2018-10-24 06:05:44 +0200

    Create config_sous_menu.php

---
+++ config_sous_menu.php
@@ -0,0 +1,65 @@
+
+	<!--
+	|	6	5	4	3	2	1	0	1	2	3	4	5	6	|	
+	+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+	|				   				   	        				|			
+	|	_______   _______    __          __     _________________    _____________      ____         ____     	|
+	|      |       \ /	 |  |  |  	|  | 	|		|    |           |      |  |         |  |       |   
+	|      |     _  V  _	 |  |  |  	|  | 	|     __________|    |___     ___|      |  |         |  |       |   
+	|      |    | \   / |	 |  |  |  	|  | 	|     |__________       |     |         |  |_________|  |       |   
+	|      |    |  \ /  |	 |  |  |  	|  | 	|________	|       |     |         |   _________   |       |   
+	|      |    |	V   |	 |  |  |________|  |   	_________|	|	|     |         |  |         |  |       |   
+	|      |    |	    |	 |  |    	   | 	|		|       |     |         |  |         |  |       |   
+	|      |____|	    |____|  |______________| 	|_______________|       |_____|         |__|         |__|       |   
+	|														|
+	+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+	-->
+	<?php
+	/****************************** MENU ***************************************************************************/
+	//$keymenu=array('nacre','rd','growth','produit','achats','dossier_financier','communication','operations')	;
+	//$valeurmenu=array('NACRE','R&D','GROWTH','PRODUIT','ACHATS','DOSSIER FINANCIER','COMMUNICATION','OPÉRATIONS')	;
+
+	//$w = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
+	//$incr =array(''.$w.'I.',''.$w.'II.',''.$w.'III.',''.$w.'IV.',''.$w.'V.',''.$w.'VI.',''.$w.'VII.',''.$w.'VIII.')	;
+
+	$key_ss_menu=array($key_nacre,$key_rd, $key_growth,$key_produit,$key_achats,$key_dossierfinancier,$key_communication,$key_operations); 
+	$valeur_ss_menu=array($valeur_nacre,$valeur_rd, $valeur_growth,$valeur_produit,$valeur_achats,$valeur_dossierfinancier,$valeur_communication,$valeur_operations); 
+
+	/****************************** NACRE **************************************************************************/
+	//$key_nacre=array('operations','developpement','growth','kpi','formalites','memorandum')			;
+	//$valeur_nacre=array('OPÉRATIONS','DÉVELOPPEMENT','GROWTH','KPI','FORMALITÉS','MEMORANDUM')			;
+	/****************************** R&D ****************************************************************************/
+	//$key_rd=array('marche','landing_page','canvas','slides')							;
+	//$valeur_rd=array('MARCHÉ','LANDING PAGE','CANVAS','SLIDES')							;
+	/****************************** GROWTH *************************************************************************/
+	//$key_commercialisation=array('acquisition','activation','revenu','recommandation','retention')		;
+	//$valeur_commercialisation=array('ACQUISITION','ACTIVATION','REVENU','RECOMMANDATION','RETENTION')		;
+
+	//$key_prospects=array('ebay','linkedin','viadeo','twitter','brandedme','googleplus')				;
+	//$valeur_prospects=array('EBAY','LINKEDIN','VIADEO','TWITTER','BRANDED ME','GOOGLE +')				;
+
+	//$key_actions_de_prospection=array('actualites','fichier_client','emailing')					;
+	//$valeur_actions_de_prospection=array('ACTUALITÉS','FICHIER CLIENT','EMAILING')				;
+	/****************************** PRODUIT ************************************************************************/
+	//$key_produit=array('clients','transport','hebergement','restauration','visite','activite')			;
+	//$valeur_produit=array('CLIENTS','TRANSPORT','HEBERGEMENT','RESTAURATION','VISITE','ACTIVITÉ')			;
+	/****************************** MARCHÉ *************************************************************************/
+	//$key_marche=array('donnees_primaires','pdv','contraintes','evolution','concurrence','reussite','partenariats');
+	//$valeur_marche=array('DONNÉES PRIMAIRES','PDV','CONTRAINTES','ÉVOLUTION','CONCURRENCE','RÉUSSITE','PARTENARIATS');
+	/****************************** ACHATS *************************************************************************/
+	//$key_achats=array('inventaire','items','produits','options','livre_journal','reservations','nomenclature')	;
+	//$valeur_achats=array('INVENTAIRE','ITEMS','PRODUITS','OPTIONS','LIVRE JOURNAL','RESERVATIONS','NOMENCLATURE')	;
+	/****************************** DOSSIER FINANCIER **************************************************************/
+	//$key_dossierfinancier=array('ventes','tresorerie','previsions','plan_comptable','transferts')			;
+	//$valeur_dossierfinancier=array('VENTES','TRÉSORERIE','PRÉVISIONS DE TRÉSORERIE','PLAN COMPTABLE','TRANSFERTS');
+	/****************************** COMMUNICATION ******************************************************************/
+	//$key_communication=array('contact','courrier','actualites','newsletter','formulaire_contact')			;
+	//$valeur_communication=array('CONTACT','COURRIER','ACTUALITES','NEWSLETTER','FORMULAIRE DE CONTACT')		;
+	/****************************** OPÉRATIONS *********************************************************************/
+	//$key_operations=array('devis','voyageurs','bi','commandes','vouchers','sav','annulation','c_d_v','facture')	;
+	//$valeur_operations=array('DEVIS','VOYAGEURS','BI','COMMANDES','VOUCHER','SAV','ANNULATION','CDV','FACTURE')	;
+	/****************************** FOOTER *************************************************************************/
+	//$key_footer=array('www.musth.eu','plan-du-site','actualites')							;
+	//$valeur_footer=array('ACCUEIL','PLAN DU SITE','ACTUALITES')							;
+
+	?>
