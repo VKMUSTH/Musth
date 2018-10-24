42003b3b388724427e7874aee8a4b62436b4bb92
VKMUSTH <valerykempf@gmail.com>
2018-10-24 04:53:36 +0200

    Delete goto_historique.php

--- growth/retention/goto_historique.php
+++
@@ -1,7 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php
-$get_numcontact = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
-$get_numcontact->execute(array('nv_numcontact' => $_POST['numcontact']));
-
-header('Location: historique');
-?>
