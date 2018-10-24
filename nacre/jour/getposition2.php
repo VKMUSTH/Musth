6b6a95321f870b645b4494411fe26731325e4576
VKMUSTH <valerykempf@gmail.com>
2018-10-24 05:11:19 +0200

    Delete getposition2.php

--- nacre/jour/getposition2.php
+++
@@ -1,7 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php
-$actualiserposition2 = $bdd->prepare('UPDATE admin SET position2 = :nv_position2 WHERE id = \'1\'');
-$actualiserposition2->execute(array('nv_position2' => $_POST['position2']	));
-
-header('Location: ../jour');
-?>
