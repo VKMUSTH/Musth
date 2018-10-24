63f1b774729d0f10a3d5ac2fd55e618250d7d62f
VKMUSTH <valerykempf@gmail.com>
2018-10-24 05:14:19 +0200

    Delete modifier.php

--- nacre/modifier.php
+++
@@ -1,7 +0,0 @@
-<?php require "../connexion_sql.php"; ?>
-<?php
-$actualiser = $bdd->prepare('UPDATE admin SET position = :nv_position WHERE id = \'1\'');
-$actualiser->execute(array('nv_position' => $_POST['position']));
-
-header('Location: modifier');
-?>
