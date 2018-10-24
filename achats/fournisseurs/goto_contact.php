af027249dd838eaa3baaef41913f3648c759620f
VKMUSTH <valerykempf@gmail.com>
2018-10-24 04:33:09 +0200

    Delete goto_contact.php

--- achats/fournisseurs/goto_contact.php
+++
@@ -1,7 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php
-$req = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
-$req->execute(array('nv_numcontact' => $_POST['numcontact'],));
-
-header('Location: ../../../communication/contact');
-?>
