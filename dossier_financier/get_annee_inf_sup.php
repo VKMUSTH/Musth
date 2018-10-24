12194615a3e9853e23ce68e8be464e9ea65c9e8d
VKMUSTH <valerykempf@gmail.com>
2018-10-24 04:46:29 +0200

    Delete get_annee_inf_sup.php

--- dossier_financier/get_annee_inf_sup.php
+++
@@ -1,8 +0,0 @@
-<?php require "../connexion_sql.php"; ?>
-<?php
-
-$set_annee_inf_sup = $bdd->prepare('UPDATE admin SET annee_en_cours = :nv_annee_en_cours WHERE id = \'1\'');
-$set_annee_inf_sup->execute(array('nv_annee_en_cours' => $_POST['annee_en_cours'],	));
-
-header('Location: ../dossier_financier');
-?>
