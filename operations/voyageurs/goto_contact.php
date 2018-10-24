5b7185206b3cd603ef300ac2945ce148047e0f95
VKMUSTH <valerykempf@gmail.com>
2018-10-24 05:32:43 +0200

    Delete goto_contact.php

--- operations/voyageurs/goto_contact.php
+++
@@ -1,9 +0,0 @@
-<?php require "../../connexion_sql.php"; ?>
-<?php 
-$req = $bdd->prepare('UPDATE admin SET numvoyageur = :nv_numvoyageur WHERE id = \'1\'');
-$req->execute(array(	'nv_numvoyageur' => $_POST['numvoyageur'], 	)); 
-
-$req = $bdd->prepare('UPDATE admin SET numcontact = :nv_numcontact WHERE id = \'1\'');
-$req->execute(array(	'nv_numcontact' => $_POST['numcontact'], 	)); 
-header('Location: ../../communication/contact');
-?>
