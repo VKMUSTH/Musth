05d2cfa073515d07a892e3d56764a51a36e2224d
VKMUSTH <valerykempf@gmail.com>
2019-05-02 15:24:22 +0200

    Create campagne.sh

---
+++ activation/campagne/campagne.sh
@@ -0,0 +1,32 @@
+	#onglet musth	151 41
+	#onglet gmail	288 37
+#PAGE MUSTH
+	#bouton+	1627 271
+	#input mail	1177 373
+	#titre		490 433
+	#texte		737 487
+#PAGE GMAIL
+	#nouveau msg	182 200
+	#destinataire	525 227
+	#titre		528 260
+	#texte		588 277	
+	#envoyer mail	290 1012
+
+xdotool mousemove 151 41
+sleep 1
+
+xdotool mousemove 288 37
+sleep 1
+
+xdotool mousemove 1627 271
+sleep 1
+
+xdotool mousemove 1177 373
+sleep 1
+
+xdotool mousemove 490 433
+sleep 1
+
+xdotool mousemove 737 487
+sleep 1
+
